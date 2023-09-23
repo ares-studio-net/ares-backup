cdd984cba8c2fcc286a11f4eb46a0a052caef0f1
410-ghost <96704503+410-ghost@users.noreply.github.com>
2023-09-23 14:34:34 +0200

    1.0

---
+++ LICENSE.md
@@ -0,0 +1,46 @@
+Modified Apache License, Version 2.0
+
+Version 1.0, [23/09/23]
+
+This license ("License") is modified to expressly prohibit commercial use of the Licensed Software. Any commercial use, including but not limited to sale, rental, distribution for profit, or use in commercial activities, is prohibited. Only non-commercial use is permitted in accordance with the terms and conditions below.
+
+In accordance with the terms of the Apache License, Version 2.0, below:
+
+1. Definitions.
+
+a. "You" or "Your" refers to the individual or entity obtaining the license for the Software under this Modified License.
+
+b. "Software" refers to the software covered by this License, including any source code, documentation, user interfaces, and any other associated materials.
+
+2. License Grant.
+
+Subject to the terms and conditions of this Modified License, the licensor grants you a worldwide, free, non-exclusive, non-transferable, and revocable license for non-commercial use of the Software for testing, experimentation, study, or research purposes, excluding any commercial use.
+
+3. Restrictions.
+
+Any commercial use of the Software is prohibited under this Modified License. You are not authorized to sell, rent, distribute for profit, or use the Software for profit.
+
+4. Waiver.
+
+This Modified License does not grant you any rights for commercial use, reproduction, or distribution of the Software for profit.
+
+5. License Notice.
+
+Any distribution of the modified Software must include a copy of this Modified License.
+
+6. Changes to Apache License, Version 2.0.
+
+You acknowledge that this Modified License has not been approved by the Apache Software Foundation and is not an official version of the Apache License, Version 2.0.
+
+7. Termination of License.
+
+The license is revocable in case of non-compliance with its terms.
+
+By accepting this Modified License, you agree to the conditions stated herein. If you do not agree with these conditions, you do not have the right to use, copy, or distribute the Software.
+
+ANY COMMERCIAL USE OF THE SOFTWARE IS EXPRESSLY PROHIBITED UNDER THIS MODIFIED LICENSE. IF YOU WISH TO USE THE SOFTWARE FOR COMMERCIAL PURPOSES, PLEASE CONTACT THE LICENSOR FOR SPECIFIC PERMISSION.
+
+For any questions regarding this Modified License, please contact [Your Name or Entity Name] at [your email address].
+
+
+[Ares-Studio.net]
---
+++ src/main/java/pl/kpgtb/backup/Backup.java
@@ -0,0 +1,141 @@
+package pl.kpgtb.backup;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public final class Backup extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        long delay = getConfig().getLong("backupsCooldown");
+        delay = delay * 60 * 60 * 20;
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                createBackup();
+            }
+        }.runTaskTimer(this,1,delay);
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    private void createBackup() {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "Creating backup");
+        File serverFile = new File(getDataFolder() + "/../../");
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                folderToZip(serverFile, Date.from(Instant.now()).toString().replace(":", " ") + ".zip");
+            }
+        }.runTaskAsynchronously(this);
+    }
+
+    private File folderToZip(File folder, String name) {
+        try {
+            File backupsFolder = new File(getDataFolder(), "backups");
+            backupsFolder.mkdirs();
+
+            purgeBackups(backupsFolder);
+
+            File zipFile = new File(backupsFolder, name);
+            zipFile.createNewFile();
+
+            FileOutputStream fos = new FileOutputStream(zipFile);
+            ZipOutputStream zos = new ZipOutputStream(fos);
+
+            addFolderToZip("", folder, zos);
+
+            zos.close();
+            fos.close();
+
+            Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "Created backup");
+            return zipFile;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void purgeBackups(File logDir){
+        File[] logFiles = logDir.listFiles();
+        long oldestDate = Long.MAX_VALUE;
+        File oldestFile = null;
+        if( logFiles != null && logFiles.length >= getConfig().getInt("maxAmountOfBackups")){
+            for(File f: logFiles){
+                if(f.lastModified() < oldestDate){
+                    oldestDate = f.lastModified();
+                    oldestFile = f;
+                }
+            }
+
+            if(oldestFile != null){
+                deleteFolder(oldestFile);
+            }
+        }
+    }
+
+    private void addFolderToZip(String parentPath, File folder, ZipOutputStream zos) throws IOException {
+        if(folder.getName().equals("backups")) {
+            return;
+        }
+
+        for (File file : folder.listFiles()) {
+            if (file.isDirectory()) {
+                String path = parentPath + file.getName() + "/";
+                ZipEntry zipEntry = new ZipEntry(path);
+                zos.putNextEntry(zipEntry);
+                addFolderToZip(path, file, zos);
+                zos.closeEntry();
+            } else {
+                try {
+                    ZipEntry zipEntry = new ZipEntry(parentPath + file.getName());
+                    zos.putNextEntry(zipEntry);
+
+                    FileInputStream fis = new FileInputStream(file);
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = fis.read(buffer)) > 0) {
+                        zos.write(buffer, 0, length);
+                    }
+
+                    fis.close();
+                    zos.closeEntry();
+                } catch (Exception e) {
+                    Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "Skipped " + file.getName() + " due to OS protection");
+                }
+            }
+        }
+    }
+
+    private void deleteFolder(File folder) {
+        if(folder == null) {
+            return;
+        }
+        if (folder.isDirectory()) {
+            // recursively delete all files and subfolders
+            for (File file : folder.listFiles()) {
+                deleteFolder(file);
+            }
+        }
+
+        // delete the folder itself
+        folder.delete();
+    }
+}
